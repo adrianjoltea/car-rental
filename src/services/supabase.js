95531ab62bf7aa10f99530be074eb525105121fb
Kingofprund <jolteaadrian@gmail.com>
2024-01-11 17:26:00 +0200

    fix: removed api key

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://phnvqjtwsbnilffyojsk.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBobnZxanR3c2JuaWxmZnlvanNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQyNzc0MzgsImV4cCI6MjAxOTg1MzQzOH0.gvwoQkGSnJ9oSMGVNtnOvjnDaR3XoDxqpLJ2NANVGi8";
+const supabaseUrl = "there was a url here";
+const supabaseKey = "there was a key here";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
